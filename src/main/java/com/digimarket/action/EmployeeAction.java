4dca1d32daeedc193901cb96a8c97d1f3cd6ed4d
chaturanand <chaturre@example.com>
2022-01-05 00:36:45 -0800

    first commit

---
+++ src/main/java/com/digimarket/action/EmployeeAction.java
@@ -0,0 +1,66 @@
+package com.digimarket.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.digimarket.listener.HibernateListener;
+import com.digimarket.model.Employee;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+/**
+ * @author Chaturanand yadav
+ * @date 2022-1-04
+ */
+
+public class EmployeeAction extends ActionSupport implements ModelDriven<Object> {
+	private static final long serialVersionUID = -7234472936649108818L;
+	Employee employee = new Employee();
+	List<Employee> empList = new ArrayList<>();
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+	public Object getModel() {
+		return employee;
+	}
+
+	// add employee in database
+	@SuppressWarnings("unchecked")
+	public String addEmployee() throws Exception {
+		SessionFactory sessionFactory = (SessionFactory) ServletActionContext.getServletContext()
+				.getAttribute(HibernateListener.KEY_NANE);
+		Session session = sessionFactory.openSession();
+
+		session.beginTransaction();
+		session.save(employee);
+		session.getTransaction().commit();
+		empList = null;
+		empList = session.createQuery("from Employee").list();
+		return SUCCESS;
+	}
+
+	// fetch employees from database
+	@SuppressWarnings("unchecked")
+	public String fetchEmployee() throws Exception {
+		SessionFactory sessionFactory = (SessionFactory) ServletActionContext.getServletContext()
+				.getAttribute(HibernateListener.KEY_NANE);
+		Session session = sessionFactory.openSession();
+		empList = session.createQuery("from Employee").list();
+		return SUCCESS;
+	}
+
+	public List<Employee> getEmpList() {
+		return empList;
+	}
+
+	public void setEmpList(List<Employee> empList) {
+		this.empList = empList;
+	}
+}
---
+++ src/main/java/com/digimarket/model/Employee.java
@@ -0,0 +1,69 @@
+package com.digimarket.model;
+
+import java.io.Serializable;
+
+/**
+ * @author Chaturanand yadav
+ * @date 2022-1-04
+ */
+public class Employee implements Serializable {
+	private static final long serialVersionUID = 4513107453540343598L;
+	// getters and setters...
+	private Long empId;
+	private String empName;
+	private String empDept;
+	private String empSalary;
+	private String empEmail;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String empName, String empDept, String empSalary, String empEmail) {
+		this.empDept = empDept;
+		this.empEmail = empEmail;
+		this.empName = empName;
+		this.empSalary = empSalary;
+	}
+
+	public Long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpDept() {
+		return empDept;
+	}
+
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+
+	public String getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(String empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	public String getEmpEmail() {
+		return empEmail;
+	}
+
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+
+}
